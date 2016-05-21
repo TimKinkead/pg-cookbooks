c33833aa96cc072caf2c43820354b755015b4f76
Derek Soike <dsoike@gmail.com>
2016-05-20 18:04:17 -0700

    Social Seed Update Cron Jobs
    Added cron jobs for categories & state/county.

--- pg-signalnoise/recipes/socialseed-cronjobs.rb
+++ pg-signalnoise/recipes/socialseed-cronjobs.rb
@@ -4,11 +4,33 @@
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-# lookup follower count for active seeds
+# lookup twitter data for social seeds
 
-cron "socialseed_update_follower_count" do
+cron "socialseed_update_twitter_data" do
   minute "0"
   hour "*"
   day "*"
-  command "wget -O - http://127.0.0.1:3000/data/socialseed/update/follower-count >/dev/null 2>&1"
+  command "wget -O - http://127.0.0.1:3000/data/socialseed/update/twitter-data >/dev/null 2>&1"
+end
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# update social seed categories from user data
+
+cron "socialseed_update_categories" do
+  minute "5"
+  hour "*"
+  day "*"
+  command "wget -O - http://127.0.0.1:3000/data/socialseed/update/categories >/dev/null 2>&1"
+end
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# update social seed state/county from user data
+
+cron "socialseed_update_geo" do
+  minute "10"
+  hour "*"
+  day "*"
+  command "wget -O - http://127.0.0.1:3000/data/socialseed/update/geo >/dev/null 2>&1"
 end
